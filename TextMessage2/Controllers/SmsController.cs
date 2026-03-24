ffec42de22a8a2e87dd4f38cfc335d07f7283cec
NicoleAndrea00 <delrosan@tcd.ie>
2026-03-24 20:46:44 +0000

    Add Text Message Lab

---
+++ TextMessage2/Controllers/SmsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TextMessageModel = TextMessage.Models.MessageText;
+
+namespace TextMessage.Controllers
+{
+    public class SmsController : Controller
+    {
+        // GET: SmsController
+        public ActionResult Send()
+        {
+            TextMessageModel message = new TextMessageModel();
+            return View(message);
+        }
+
+        // POST: SmsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Send(TextMessageModel message)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirmation", message);
+            }
+            return View(message);
+
+        }
+        public ActionResult Confirmation(TextMessageModel message)
+        {
+            return View(message);
+        }
+    }
+}
---
+++ TextMessage2/Models/MessageText.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextMessage.Models
+{
+    public class MessageText
+    {
+        [Required]
+        [StringLength(10, MinimumLength =10, ErrorMessage ="Phone Number must be exactly 10 digits long")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone Number must be 10 Digits Long.")]
+        [Display(Name ="Phone Number")]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        [StringLength(140, ErrorMessage="Message exceeded length of 140 Characters")]
+        [Display(Name ="Your Message")]
+        public string Content { get; set; }
+    }
+}
